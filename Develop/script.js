74f630ca9dd197d4fd3ae37c500d3e0d8eb60867
shawn <srclarke777@gmail.com>
2023-09-15 09:03:03 -0700

    adding in code to make the generator work

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+ let pass='';
+ let str='ABCDEFGHIJKLMNOPQRSTUVWXYZ'+'abcdefghijklmnopqrstuvwxyz012345678900!@#$%^&*()_+'
+let passwordlength=8; 
+ for (let i = 1; i<=passwordlength; i++){
+  let char =  Math.floor(Math.random() *
+  str.length +1);
+
+  pass += str.charAt(char)
+ }
+return pass;
+
+}
+
+
+// Write password to the #password input  
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
